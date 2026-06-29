3dd3f4bc121e7edefb93e3c73f05fa92a263bda6
asinghka <singhkalra.arjun@gmail.com>
2026-06-29 23:24:52 +0200

    create vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use ash_bootstrap::InstanceBuilder;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use winit::window::{Window, WindowId};
+
+fn init_window(event_loop: &ActiveEventLoop) -> anyhow::Result<Window> {
+    let window = event_loop.create_window(Window::default_attributes())?;
+
+    let window_handle = window.window_handle()?;
+    let display_handle = window.display_handle()?;
+
+    let _instance = InstanceBuilder::new(Some((window_handle, display_handle)))
+        .app_name("Vulkan Renderer")
+        .request_validation_layers(true)
+        .use_default_debug_messenger()
+        .build()?;
+
+    Ok(window)
+}
 
 #[derive(Default)]
 struct App {
@@ -10,7 +27,14 @@ struct App {
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+        match init_window(event_loop) {
+            Ok(window) => {
+                self.window = Some(window);
+            }
+            Err(err) => {
+                panic!("{:?}", err);
+            }
+        }
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
