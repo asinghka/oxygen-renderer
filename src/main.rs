25ab3dc2139e124552ffe8718c1e2a403d555c92
asinghka <singhkalra.arjun@gmail.com>
2026-06-30 21:57:48 +0200

    create instance, adapter, device & queue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use pollster::FutureExt;
+use std::sync::Arc;
+use wgpu::{Backends, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor, PowerPreference, Queue, RequestAdapterOptions};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
@@ -7,14 +10,51 @@ const APP_NAME: &str = "wgpu-renderer";
 
 #[derive(Default)]
 struct App {
-    window: Option<Window>,
+    window: Option<Arc<Window>>,
+    device: Option<Device>,
+    queue: Option<Queue>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = event_loop.create_window(Window::default_attributes().with_title(APP_NAME)).unwrap();
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes().with_title(APP_NAME))
+                .expect("Failed to create window"),
+        );
+
+        let instance = Instance::new(InstanceDescriptor {
+            backends: Backends::PRIMARY,
+            flags: Default::default(),
+            memory_budget_thresholds: Default::default(),
+            backend_options: Default::default(),
+            display: None,
+        });
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                power_preference: PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: None,
+            })
+            .block_on()
+            .expect("Failed to create an adapter");
+
+        let (device, queue) = adapter
+            .request_device(&DeviceDescriptor {
+                label: None,
+                required_features: Features::empty(),
+                required_limits: Default::default(),
+                experimental_features: Default::default(),
+                memory_hints: Default::default(),
+                trace: Default::default(),
+            })
+            .block_on()
+            .expect("Failed to create a device");
 
         self.window = Some(window);
+        self.device = Some(device);
+        self.queue = Some(queue);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
@@ -28,10 +68,12 @@ impl ApplicationHandler for App {
 }
 
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
+    env_logger::init();
+
+    let event_loop = EventLoop::new().expect("Failed to create event loop");
 
     event_loop.set_control_flow(ControlFlow::Poll);
 
     let mut app = App::default();
-    event_loop.run_app(&mut app).unwrap();
+    event_loop.run_app(&mut app).expect("Failed to run app");
 }
