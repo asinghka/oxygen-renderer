a58f0dcf73a0b2988c8fcd998b0b69cbd707b403
asinghka <singhkalra.arjun@gmail.com>
2026-06-30 19:26:41 +0200

    switch to wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,20 @@
-use ash::vk;
-use ash_bootstrap::{Instance, InstanceBuilder};
-use std::sync::Arc;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use winit::window::{Window, WindowId};
+
+const APP_NAME: &str = "wgpu-renderer";
 
 #[derive(Default)]
 struct App {
     window: Option<Window>,
-    instance: Option<Arc<Instance>>,
-}
-
-impl App {
-    fn init_window(&mut self, event_loop: &ActiveEventLoop) -> anyhow::Result<()> {
-        let window = event_loop.create_window(Window::default_attributes())?;
-
-        // ash-bootstrap welds the window's surface into the instance, so we build it only once and
-        // never on a second `resumed`; safe only because desktop (macOS/Windows) fires `resumed` once.
-        if self.instance.is_none() {
-            let window_handle = window.window_handle()?;
-            let display_handle = window.display_handle()?;
-
-            let instance = InstanceBuilder::new(Some((window_handle, display_handle)))
-                .app_name("Vulkan Renderer")
-                .require_api_version(vk::API_VERSION_1_3)
-                .request_validation_layers(cfg!(debug_assertions))
-                .use_default_debug_messenger()
-                .build()?;
-            self.instance = Some(instance);
-        }
-
-        self.window = Some(window);
-
-        Ok(())
-    }
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        match self.init_window(event_loop) {
-            Ok(_) => {}
-            Err(err) => {
-                panic!("{:?}", err);
-            }
-        }
+        let window = event_loop.create_window(Window::default_attributes().with_title(APP_NAME)).unwrap();
+
+        self.window = Some(window);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
@@ -55,14 +24,6 @@ impl ApplicationHandler for App {
             }
             _ => {}
         };
-    }
-}
-
-impl Drop for App {
-    fn drop(&mut self) {
-        if let Some(instance) = self.instance.take() {
-            instance.destroy();
-        }
     }
 }
 
