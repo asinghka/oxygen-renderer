cbe8c31a2a2caac1e7cefbc27d488043bbecdb1c
asinghka <singhkalra.arjun@gmail.com>
2026-07-07 21:43:12 +0200

    remove left panel

--- src/editor.rs
+++ src/editor.rs
@@ -2,10 +2,6 @@ use egui::Frame;
 use egui::load::SizedTexture;
 
 pub(crate) fn build(ui: &mut egui::Ui, texture_id: egui::TextureId) -> egui::Rect {
-    egui::Panel::left("panel").show(ui, |ui| {
-        ui.take_available_space();
-    });
-
     egui::CentralPanel::default()
         .frame(Frame::NONE)
         .show(ui, |ui| ui.image(SizedTexture::new(texture_id, ui.available_size())).rect)
